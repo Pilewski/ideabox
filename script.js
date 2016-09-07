af2105e81188f8f27b6db940a2f6c7a3c2b6877b
Alex Pilewski <pilewski.alex@gmail.com>
2016-09-07 16:22:58 -0600

    repopulates idea list on page load

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var $ideaTitleInput = $('#idea-title-input');
 var $ideaBodyInput = $('#idea-body-input');
 var $form = $('#input-form');
 var $ideaList = $('#idea-list');
-var arrayIdeaIDs = [];
+// var arrayIdeaIDs = [];
+// localStorage.setItem('ideaArray', JSON.stringify(arrayIdeaIDs));
 
 function newUniqueID () {
   return Date.now();
@@ -70,8 +71,7 @@ function pushNewID(id) {
 }
 
 function updateIDArray(id) {
-  var idArrayRAW = localStorage.getItem('ideaArray');
-  arrayIdeaIDs = JSON.parse(idArrayRAW);
+  var arrayIdeaIDs = retrieveIDarray();
   arrayIdeaIDs.push(id);
   localStorage.setItem('ideaArray', JSON.stringify(arrayIdeaIDs));
 }
@@ -80,3 +80,41 @@ function retrieveIdea(id) {
   var storedIdea = localStorage.getItem(id);
   return JSON.parse(storedIdea);
 }
+
+function retrieveIDarray() {
+  return JSON.parse(localStorage.getItem('ideaArray'));
+}
+// on page load
+$(window).load(function (){
+// pull array of ids from local storage
+var arrayIdeaIDs = retrieveIDarray();
+// sort array (by date created)
+arrayIdeaIDs.sort();
+  // iterate through id array (for loop)
+   for (var i = 0; i < arrayIdeaIDs.length; i++) {
+     // retrieve idea objects from ids
+     var existingIdea = retrieveIdea(arrayIdeaIDs[i]);
+     $ideaList.prepend(
+       "<li>"+
+         "<div class='idea-header'>"+
+           "<h2>"+existingIdea.title+"</h2>"+
+           "<button type='button' class='delete-btn'>"+
+           "<img src='./imgs/delete.svg' /></button>"+
+         "</div>"+
+
+       "<div class='idea-body'>"+
+         "<p>"+existingIdea.body+"</p>"+
+       "</div>"+
+
+       "<div class='idea-footer'>"+
+         "<button type='button' class='upvote-btn'><img src='./imgs/upvote.svg'/></button>"+
+         "<button type='button' class='downvote-btn'><img src='./imgs/downvote.svg' /></button>"+
+         "<article class='idea-quality'>"+
+           existingIdea.quality+
+         "</article>"+
+       "</div>"+
+     "</li>"
+     );
+   }
+  // build idea-list
+});
