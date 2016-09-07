9d6bc86677aed5ac86e25e72da00d1acfe083c8b
Alex Pilewski <pilewski.alex@gmail.com>
2016-09-07 13:58:36 -0600

    global variables

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+var $ideaTitleInput = $('#idea-title-input');
+var $ideaBodyInput = $('#idea-body-input');
 
 function newUniqueID () {
   return Date.now();
@@ -9,4 +10,12 @@ function Idea(id, title, body, quality) {
   this.title = title;
   this.body = body;
   this.quality = quality;
+}
+
+function getUserTitle () {
+  return $ideaTitleInput.value;
+}
+
+function getUserBody () {
+  return $inputBodyInput.value;
 }
