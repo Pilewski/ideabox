8d372cedf624a6028796aecd1992136de20a445b
Alex Pilewski <pilewski.alex@gmail.com>
2016-09-07 14:13:14 -0600

    create idea

--- script.js
+++ script.js
@@ -6,21 +6,29 @@ function newUniqueID () {
   return Date.now();
 }
 
-function Idea(id, title, body, quality) {
+function Idea(id, title, body) {
   this.id = id;
   this.title = title;
   this.body = body;
-  this.quality = quality;
+  this.quality = 'swill';
 }
 
 function getUserTitle () {
-  return $ideaTitleInput.value;
+  return $ideaTitleInput.val();
 }
 
 function getUserBody () {
-  return $inputBodyInput.value;
+  return $ideaBodyInput.val();
 }
 
-$form.submit( function(){
 
+$form.submit( function(){
+debugger;
+  // Create new Idea object
+  var newIdea = new Idea(newUniqueID(), getUserTitle(), getUserBody());
+  debugger;
+
+  // Add to page
+  // new function () Convert to JSON
+  // new function () Add to localStorage
 });
